fb2a5aea1edd0f60d46eae4e5ce04d8825efe724
Andreas Kissner <andikiss500@gmail.com>
2025-05-26 10:33:46 +0200

    added Card Style/Now logig with Javascript

---
+++ script.js
@@ -0,0 +1,55 @@
+// console.table(questions[0]["answer_3"]); 
+
+let questions =[
+  {
+  "question": "Welches dieser Lebensmittel enthält am meisten Vitamin C?",
+  "answer_1": "Paprika",
+  "answer_2": "Apfel",
+  "answer_3": "Gurke",
+  "right_answer": 1
+},
+{
+  "question": "Was ist eine gesunde Fettquelle?",
+  "answer_1": "Avocado",
+  "answer_2": "Butter",
+  "answer_3": "Schmalz",
+  "right_answer": 1
+},
+{
+  "question": "Was ist ballaststoffreich und fördert die Verdauung?",
+  "answer_1": "Weißbrot",
+  "answer_2": "Vollkornprodukte",
+  "answer_3": "Zucker",
+  "right_answer": 2
+},
+{
+  "question": "Welches Getränk ist am besten für den Körper?",
+  "answer_1": "Limonade",
+  "answer_2": "Eistee",
+  "answer_3": "Wasser",
+  "right_answer": 3
+},
+{
+  "question": "Welches Mineral ist wichtig für starke Knochen?",
+  "answer_1": "Kalzium",
+  "answer_2": "Natrium",
+  "answer_3": "Zink",
+  "right_answer": 1
+},
+{
+  "question": "Wie viele Portionen Obst und Gemüse werden täglich empfohlen?",
+  "answer_1": "2",
+  "answer_2": "5",
+  "answer_3": "8",
+  "right_answer": 2
+},
+{
+  "question": "Welcher dieser Begriffe steht für eine gesunde Eiweißquelle?",
+  "answer_1": "Frittierte Hähnchenteile",
+  "answer_2": "Quinoa",
+  "answer_3": "Zuckermais",
+  "right_answer": 2
+}
+];
+
+console.table(questions);
