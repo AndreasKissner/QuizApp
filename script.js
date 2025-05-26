53de8f9ba47272f86f8c6579a16218aa4e026d66
Andreas Kissner <andikiss500@gmail.com>
2025-05-26 11:01:55 +0200

    Added Question and Answers in Card an Current Question number to

--- script.js
+++ script.js
@@ -1,55 +1,81 @@
 // console.table(questions[0]["answer_3"]); 
 
-let questions =[
-  {
+let questions = [
+    {
   "question": "Welches dieser Lebensmittel enthält am meisten Vitamin C?",
-  "answer_1": "Paprika",
-  "answer_2": "Apfel",
-  "answer_3": "Gurke",
-  "right_answer": 1
+  "answer_1": "Apfel",
+  "answer_2": "Gurke",
+  "answer_3": "Paprika",
+  "answer_4": "Banane",
+  "right_answer": 3
 },
 {
   "question": "Was ist eine gesunde Fettquelle?",
-  "answer_1": "Avocado",
+  "answer_1": "Schmalz",
   "answer_2": "Butter",
-  "answer_3": "Schmalz",
-  "right_answer": 1
+  "answer_3": "Palmöl",
+  "answer_4": "Avocado",
+  "right_answer": 4
 },
 {
   "question": "Was ist ballaststoffreich und fördert die Verdauung?",
-  "answer_1": "Weißbrot",
-  "answer_2": "Vollkornprodukte",
-  "answer_3": "Zucker",
+  "answer_1": "Zucker",
+  "answer_2": "Vollkornbrot",
+  "answer_3": "Weißbrot",
+  "answer_4": "Käse",
   "right_answer": 2
 },
 {
   "question": "Welches Getränk ist am besten für den Körper?",
-  "answer_1": "Limonade",
-  "answer_2": "Eistee",
-  "answer_3": "Wasser",
-  "right_answer": 3
+  "answer_1": "Wasser",
+  "answer_2": "Limonade",
+  "answer_3": "Eistee",
+  "answer_4": "Cola Light",
+  "right_answer": 1
 },
 {
   "question": "Welches Mineral ist wichtig für starke Knochen?",
-  "answer_1": "Kalzium",
-  "answer_2": "Natrium",
+  "answer_1": "Eisen",
+  "answer_2": "Kalzium",
   "answer_3": "Zink",
-  "right_answer": 1
+  "answer_4": "Magnesium",
+  "right_answer": 2
 },
 {
   "question": "Wie viele Portionen Obst und Gemüse werden täglich empfohlen?",
   "answer_1": "2",
-  "answer_2": "5",
-  "answer_3": "8",
-  "right_answer": 2
+  "answer_2": "3",
+  "answer_3": "5",
+  "answer_4": "7",
+  "right_answer": 3
 },
 {
   "question": "Welcher dieser Begriffe steht für eine gesunde Eiweißquelle?",
   "answer_1": "Frittierte Hähnchenteile",
-  "answer_2": "Quinoa",
-  "answer_3": "Zuckermais",
-  "right_answer": 2
+  "answer_2": "Wurst",
+  "answer_3": "Quinoa",
+  "answer_4": "Zuckermais",
+  "right_answer": 3
 }
+
 ];
 
-console.table(questions);
+
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById("total_questions").innerHTML = questions.length;
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+    
+    document.getElementById("question_text").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+
+}
